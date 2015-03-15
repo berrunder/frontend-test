80aaba67355fe9c45c9c6c975e562845e388fc6b
Alexander Cherkashin <alexander.cherkashin@starcode.ru>
2015-03-15 15:18:26 +0300

    refactoring

--- index.php
+++ index.php
@@ -16,14 +16,20 @@ $app->get('/', function () use ($app) {
 
 $app->post('/product', function () use ($app) {
     $body = $app->request->getBody();
-    $result = saveJson($body);
+    $result = Product::save(json_decode($body));
+
+    if ($result['success']) {
+        $app->response->setStatus(201);
+    } else {
+        $app->response->setStatus(500);
+    }
 
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($result));
 });
 
 $app->get('/product', function() use ($app) {
-    $products = getProducts();
+    $products = Product::getAll();
     $app->response->headers->set('Content-Type', 'application/json');
     $app->response->setBody(json_encode($products));
 });
@@ -37,47 +43,3 @@ $app->delete('/product/:id', function($id) use ($app) {
 });
 
 $app->run();
-
-function saveJson($raw)
-{
-    $data = json_decode($raw);
-
-    if ($data) {
-        $data->submitted = time() * 1000;
-        $files = array_filter(glob(DATA_DIR . DIRECTORY_SEPARATOR . '*.json'), 'is_file');
-        $num = 1;
-        foreach ($files as $file) {
-            $fileId = intval(basename($file, ".json"));
-            if ($num <= $fileId) {
-                $num = $fileId + 1;
-            }
-        }
-
-        if (file_put_contents(DATA_DIR . DIRECTORY_SEPARATOR . $num . '.json', json_encode($data)) !== false) {
-            return array(
-                'success' => true,
-                'id' => $num,
-                'submitted' => $data->submitted,
-            );
-        }
-    }
-
-    return array('success' => false);
-}
-
-function getProducts() {
-    $products = [];
-    $files = array_filter(glob(DATA_DIR . DIRECTORY_SEPARATOR . '*.json'), 'is_file');
-
-    foreach ($files as $file) {
-        $fileId = intval(basename($file, ".json"));
-        $raw = file_get_contents($file);
-        $data = json_decode($raw);
-        if ($data && is_object($data)) {
-            $data->id = $fileId;
-            $products[] = $data;
-        }
-    }
-
-    return $products;
-}
--- src/Product.php
+++ src/Product.php
@@ -7,4 +7,47 @@ class Product {
     {
         return unlink(DATA_DIR . DIRECTORY_SEPARATOR . "$id.json");
     }
+
+    public static function save($product)
+    {
+        if ($product) {
+            $product->submitted = time() * 1000;
+            $files = array_filter(glob(DATA_DIR . DIRECTORY_SEPARATOR . '*.json'), 'is_file');
+            $num = 1;
+            foreach ($files as $file) {
+                $fileId = intval(basename($file, ".json"));
+                if ($num <= $fileId) {
+                    $num = $fileId + 1;
+                }
+            }
+
+            if (file_put_contents(DATA_DIR . DIRECTORY_SEPARATOR . $num . '.json', json_encode($product)) !== false) {
+                return array(
+                    'success' => true,
+                    'id' => $num,
+                    'submitted' => $product->submitted,
+                );
+            }
+        }
+
+        return array('success' => false);
+    }
+
+    public static function getAll()
+    {
+        $products = [];
+        $files = array_filter(glob(DATA_DIR . DIRECTORY_SEPARATOR . '*.json'), 'is_file');
+
+        foreach ($files as $file) {
+            $fileId = intval(basename($file, ".json"));
+            $raw = file_get_contents($file);
+            $data = json_decode($raw);
+            if ($data && is_object($data)) {
+                $data->id = $fileId;
+                $products[] = $data;
+            }
+        }
+
+        return $products;
+    }
 }
